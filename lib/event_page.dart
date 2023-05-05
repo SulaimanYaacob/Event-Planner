0b5143d522bdf7564a16c8b0c580f377f79f855b
Sulaiman Yaacob <davidjethro01@gmail.com>
2023-05-05 19:13:54 +0800

    add drawer and event gridtile

--- lib/event_page.dart
+++ lib/event_page.dart
@@ -14,8 +14,80 @@ class _EventPageState extends State<EventPage> {
         appBar: AppBar(
           title: const Text('Events'),
         ),
-        body: const Center(
-          child: Text('Event'),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const UserAccountsDrawerHeader(
+                accountName: Text('John Doe'),
+                accountEmail: Text('johndoe@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.amberAccent,
+                  child: Icon(
+                    Icons.local_fire_department_outlined,
+                    color: Colors.red,
+                    size: 50,
+                  ),
+                ),
+              ),
+              //TODO Create its own widget
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () => {
+                  //Navigate to different page
+                },
+                autofocus: true,
+              ),
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: [
+              Stack(
+                children: [
+                  GridTile(
+                    footer: Material(
+                      color: Colors.transparent,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                              bottom: Radius.circular(4.0))),
+                      clipBehavior: Clip.antiAlias,
+                      child: InkWell(
+                        onTap: () => debugPrint('Event 1'),
+                        child: GridTileBar(
+                          title: const Text('Event 1'),
+                          subtitle: const Text('Subtitle'),
+                          backgroundColor: Colors.black45,
+                          trailing: IconButton(
+                            splashRadius: 20.0,
+                            icon: const Icon(Icons.favorite_border),
+                            onPressed: () => {
+                              debugPrint('Favorite'),
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    child: Semantics(
+                      child: Material(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                        clipBehavior: Clip.antiAlias,
+                        child: const Image(
+                          image: NetworkImage(
+                              'https://picsum.photos/250?image=53'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ));
   }
 }

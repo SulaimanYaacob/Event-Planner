8d10ab6206f8a0dd4094d8ce5f9fdcf17ed0e9cb
OmarMokhtarYohan <omarmokhtar@graduate.utm.my>
2023-05-20 06:41:06 +0800

    add if else check to check if sign in

--- lib/home/wrapper.dart
+++ lib/home/wrapper.dart
@@ -1,13 +1,22 @@
 import 'package:event_planner/authenticate/authenticate.dart';
 import 'package:event_planner/main.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:event_planner/models/user.dart' as Planner;
 
 class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     
-    return Authenticate();
+    final user = Provider.of<Planner.User?>(context);
+    
+    // return either the Home or Authenticate widget
+    if (user == null){
+      return Authenticate();
+    } else {
+      return HomePage();
+    }
 
   }
 }
